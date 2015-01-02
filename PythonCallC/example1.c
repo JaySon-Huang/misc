9b5e2b03a915faf18575c102d45b0d8f80162447
JaySon <tshent@qq.com>
2015-01-02 17:40:59 +0800

    返回python列表、字典

--- PythonCallC/example1.c
+++ PythonCallC/example1.c
@@ -11,5 +11,39 @@ int hello1()
 
 PyObject *hello2(PyObject *self, PyObject *args)
 {
+    // https://docs.python.org/2/c-api/tuple.html
+    // https://docs.python.org/2/c-api/arg.html?highlight=py_buildvalue#c.Py_BuildValue
     return Py_BuildValue("(iis)", 1, 2, "three");
+}
+
+PyObject *hello3(PyObject *self, PyObject *args)
+{
+    PyObject *list = PyList_New(0);
+    PyObject *item = NULL;
+    for (int i=0; i!=5; ++i){
+        PyObject *sublist = PyList_New(0);
+        for (int j=0; j!=64; ++j){
+            PyList_Append(sublist, Py_BuildValue("i", i*100+j));
+        }
+        PyList_Append(list, sublist);
+    }
+    return list;
+}
+
+PyObject *hello4(PyObject *self, PyObject *args)
+{
+    PyObject *dict = PyDict_New();
+
+    PyObject *list = PyList_New(0);
+    PyObject *item = NULL;
+    for (int i=0; i!=5; ++i){
+        PyObject *sublist = PyList_New(0);
+        for (int j=0; j!=64; ++j){
+            PyList_Append(sublist, Py_BuildValue("i", i*100+j));
+        }
+        PyList_Append(list, sublist);
+    }
+    PyDict_SetItem(dict, Py_BuildValue("s", "Y"), list);
+
+    return dict;
 }
--- PythonCallC/pyex.py
+++ PythonCallC/pyex.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from pprint import pprint as pp
+
 import ctypes
 so = ctypes.CDLL("./libhello.so")
 ret = so.hello1()
@@ -7,4 +9,16 @@ print hex(ret)
 hellofunc = so.hello2
 hellofunc.restype = ctypes.py_object
 ret = hellofunc()
+print ret
+print dir(hellofunc)
+
+hello3func = so.hello3
+hello3func.restype = ctypes.py_object
+ret = hello3func()
+for l in ret:
+    print l
+
+hello4func = so.hello4
+hello4func.restype = ctypes.py_object
+ret = hello4func()
 print ret
