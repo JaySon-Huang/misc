ebd680299fe48f375cbd3fc670f39b2d18b39d39
JaySon <tshent@qq.com>
2015-01-03 04:16:10 +0800

    use python3 to compile this

--- PythonCallC/example3.c
+++ PythonCallC/example3.c
@@ -3,19 +3,35 @@
 
 #include <Python/Python.h>
 
+#include <assert.h>
+
+#define LIST_SIZE 300
+
 PyObject *hello4(PyObject *self, PyObject *args)
 {
     PyObject *dict = PyDict_New();
-
-    PyObject *list = PyList_New(4640);
+    assert(dict != NULL);
+    printf("dict is NOT NULL.\n");
+    PyObject *list = PyList_New(LIST_SIZE);
+    assert(list != NULL);
+    printf("list is NOT NULL.\n");
     int cnt=0;
-    for (int i=0; i!=4640; ++i){
-        PyObject *sublist = PyList_New(64);
+    PyObject *sublist;
+    for (int i=0; i!=LIST_SIZE; ++i){
+        printf("Creating new sublist...\n");
+        sublist = PyList_New(64);
+        assert(sublist != NULL);
+        printf("sublist is NOT NULL.\n");
         for (int j=0; j!=64; ++j){
-            PyList_SetItem(sublist, j, Py_BuildValue("i", i*100+j));
+            PyObject *val = Py_BuildValue("i", i*100+j);
+            PyList_SetItem(sublist, j, val);
+            // Py_DECREF(val);
         }
         printf("%d\n", cnt++);
         PyList_SetItem(list, i, sublist);
+        // Py_DECREF(sublist);
+
+        printf("sublist added to list\n");
     }
     PyDict_SetItem(dict, Py_BuildValue("s", "Y"), list);
 
--- PythonCallC/pyex3.py
+++ PythonCallC/pyex3.py
@@ -3,4 +3,4 @@ so = ctypes.CDLL("./libhello.so")
 hello4func = so.hello4
 hello4func.restype = ctypes.py_object
 ret = hello4func()
-print ret
+print(ret)
