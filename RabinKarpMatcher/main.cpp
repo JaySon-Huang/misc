9a944e61c6e62e37eddd00cf890ff4aec1d2b6c8
JaySon-Huang <jayson.hjs@gmail.com>
2015-07-15 17:25:09 +0800

    use gtest for unittest

--- RabinKarpMatcher/main.cpp
+++ RabinKarpMatcher/main.cpp
@@ -10,73 +10,123 @@ using namespace std;
 #include "CRabinKarpMatcher.h"
 #include "CSimpleMatcher.h"
 
-int testMatcher(CBaseMatcher *matcher)
+#include "gtest/gtest.h"
+
+struct STTestSentence
+{
+public:
+    string m_sentence;
+    bool m_isMatched;
+    size_t m_posMatched;
+    STTestSentence(
+        const char *sentence, bool is_matched,
+        size_t pos_matched
+    ): m_sentence(sentence), m_isMatched(is_matched), m_posMatched(pos_matched)
+    {}
+};
+
+class MatcherTest : public testing::TestWithParam<STTestSentence>
+{
+public:
+    static vector<STTestSentence> sentences;
+
+    static vector<STTestSentence>& SetUpTestSentences() {
+        // 英文
+        sentences.push_back(STTestSentence("HELL", true, 0));
+        sentences.push_back(STTestSentence(" HELL", true, 1));
+        sentences.push_back(STTestSentence(" HELL ", true, 1));
+        sentences.push_back(STTestSentence("ello world", true, 5));
+        sentences.push_back(STTestSentence("Hello World", false, 0));
+        // 中文
+        sentences.push_back(STTestSentence("血界战线", true, 0));
+        sentences.push_back(STTestSentence(" 血界战线", true, 1));
+        sentences.push_back(STTestSentence("看血界战线", true, 3));
+        sentences.push_back(STTestSentence("看幻界战线", false, 0));
+        sentences.push_back(STTestSentence(
+            "简短地生成随机密码,包括大小写字母、数字,可以指定密码长度'''#生成随机",
+            false, 0)
+        );
+        sentences.push_back(STTestSentence(
+            "Python面试题笔试题:标准库random实现了一个随机数生成器,实例代码如下",
+            true, 0)
+        );
+        sentences.push_back(STTestSentence(
+            "上一篇:使用Python批量替换指定目录所有文件中的指定文本 下一篇",
+            true, 16)
+        );
+        sentences.push_back(STTestSentence(
+            "文本文档是以TXT后缀名的文件，在windows页面重点及鼠标右键-〉新建-〉在下拉子菜单中有文本文档一项。 详细：txt文件是微软在操作系统上附带的一种文本格式，是最常见的一种文件格式，早在DOS时代应用就很多，主要存文本信息，即为文字信.",
+            false, 0)
+        );
+        return sentences;
+    }
+protected:
+    static CBaseMatcher *rk_matcher;
+    static CBaseMatcher *s_matcher;
+public:
+    static void SetUpTestCase() {
+        const static unsigned int NUM_KEYWORD = 7;
+        const static char *KEYWORDS [NUM_KEYWORD] = {
+            "hello", "world", "Python",
+            "HELL", "WORLD", "Django",
+            "血界战线",
+        };
+
+        rk_matcher = new CRabinKarpMatcher(KEYWORDS, NUM_KEYWORD);
+        s_matcher = new CSimpleMatcher(KEYWORDS, NUM_KEYWORD);
+    }
+
+    static void TearDownTestCase() {
+        delete rk_matcher;
+        delete s_matcher;
+    }
+};
+CBaseMatcher *MatcherTest::rk_matcher = NULL;
+CBaseMatcher *MatcherTest::s_matcher = NULL;
+vector<STTestSentence> MatcherTest::sentences;
+
+TEST_P(MatcherTest, Simple)
 {
     size_t index_matched = -1;
     string string_matched;
-    static const int NUM_SMAPLES = 16;
-    const char *samples_to_match [NUM_SMAPLES] = {
-        "HELL", " HELL", " HELL ",
-        "What the HELL ", "Hello", "hello", "ello world",
-        "血界战线", "=血界战线", "看血界战线!", "强烈推荐血界战线!!",
-        "首先看一下来自Wolfram的定义马尔可夫链是随机变量{X_t}的集合（t贯穿0,1,...），给定当前的状态，未来与过去条件独立。Wikipedia的定义更清楚一点儿...马尔可夫链是具有马尔可夫性质的随机过程...[这意味着]状态改变是概率性的，未来的状态仅仅依赖当前的状态。马尔可夫链具有多种用途，现在让我看一下如何用它生产看起来像模像样的胡言乱语。算法如下,找一个作为语料库的文本，语料库用于选择接下来的转换。从文本中两个连续的单词开始，最后的两个单词构成当前状态。生成下一个单词的过程就是马尔可夫转换。为了生成下一个单词，首先查看语料库，查找这两个单词之后跟着的单词。从它们中随机选择一个。重复2，直到生成的文本达到需要的大小。",
-        "简短地生成随机密码,包括大小写字母、数字,可以指定密码长度'''#生成随机",
-        "Python面试题笔试题:标准库random实现了一个随机数生成器,实例代码如下",
-        "上一篇:使用Python批量替换指定目录所有文件中的指定文本 下一篇",
-        "文本文档是以TXT后缀名的文件，在windows页面重点及鼠标右键-〉新建-〉在下拉子菜单中有文本文档一项。 详细：txt文件是微软在操作系统上附带的一种文本格式，是最常见的一种文件格式，早在DOS时代应用就很多，主要存文本信息，即为文字信.",
-    };
-    for (int i = 0; i!= NUM_SMAPLES; ++i)
-    {
-        matcher->match(samples_to_match[i],
-            &string_matched, &index_matched
-        );
-        // fprintf(stdout,
-        //     "`%s`:\nPattern [%s] occurs in offset [%lu]\n",
-        //     samples_to_match[i],
-        //     string_matched.c_str(), index_matched
-        // );
-    }
 
-    return 0;
+    STTestSentence sentence = GetParam();
+
+    EXPECT_EQ(sentence.m_isMatched,
+        s_matcher->match(sentence.m_sentence, &string_matched, &index_matched));
+    if (sentence.m_isMatched)
+    {
+        EXPECT_EQ(sentence.m_posMatched, index_matched)
+            << "matched string:`" << string_matched << "` @ " << index_matched;
+    }
 }
 
-int main(int argc, char const *argv[])
+TEST_P(MatcherTest, RabinKarp)
 {
-    const char *keywords [6] = {
-        "hello", "world", 
-        "HELL", "WORLD",
-        "血界战线", "吹响!上悠风号",
-    };
+    size_t index_matched = -1;
+    string string_matched;
 
-    clock_t start, end;
-    static const unsigned int TEST_ROUND = 100;
-    CBaseMatcher *matcher = NULL;
+    STTestSentence sentence = GetParam();
 
-    cout << "testing CRabinKarpMatcher..." << endl;
-    start = clock();
-    matcher = new CRabinKarpMatcher(keywords, 6);
-    for (int i=0; i!=TEST_ROUND; ++i)
+    EXPECT_EQ(sentence.m_isMatched,
+        rk_matcher->match(sentence.m_sentence, &string_matched, &index_matched));
+    if (sentence.m_isMatched)
     {
-        testMatcher(matcher);
+        EXPECT_EQ(sentence.m_posMatched, index_matched)
+            << "matched string:`" << string_matched << "` @ " << index_matched;
     }
-    delete matcher;
-    end = clock();
-    fprintf(stdout, "Cost %.4f secs for %d loops\n",
-        ((double)(end-start)/CLOCKS_PER_SEC), TEST_ROUND);
+}
 
-    cout << endl;
 
-    cout << "testing CSimpleMatcher..." << endl;
-    start = clock();
-    matcher = new CSimpleMatcher(keywords, 6);
-    for (int i=0; i!=TEST_ROUND; ++i)
-    {
-        testMatcher(matcher);
-    }
-    delete matcher;
-    end = clock();
-    fprintf(stdout, "Cost %.4f secs for %d loops\n",
-        ((double)(end-start)/CLOCKS_PER_SEC), TEST_ROUND);
+INSTANTIATE_TEST_CASE_P(
+    No, MatcherTest,
+    testing::ValuesIn(MatcherTest::sentences)
+);
 
-    return 0;
+int main(int argc, char **argv)
+{
+    MatcherTest::SetUpTestSentences();
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
